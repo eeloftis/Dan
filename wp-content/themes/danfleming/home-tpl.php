8fe4c19460784d46fd950b1e2b41b8384ead1de1
Ema Loftis <eloftis@wellesley.edu>
2017-07-29 23:09:21 -0400

    Added images to project cards

--- wp-content/themes/danfleming/home-tpl.php
+++ wp-content/themes/danfleming/home-tpl.php
@@ -54,13 +54,13 @@ the_post(); ?>
             </div>
           </div>
           <div class="col-lg-5 col-lg-offset-0 col-xs-10 col-xs-offset-1"><img src="<?php
-         echo get_field('home_logo')['url'];
+         echo get_field('home_image')['url'];
          ?>" ></div>
          </div>
        <?php else: ?>
          <div class="projectCard odd row">
            <div class="col-xs-10 col-xs-offset-1 col-lg-5 col-lg-offset-0 col-md-5 col-md-offset-0 col-sm-5 col-sm-offset-0"><img src="<?php
-         	echo get_field('home_logo')['url'];
+         	echo get_field('home_image')['url'];
          	?>" ></div>
            <div class="col-xs-10 col-xs-offset-1 col-lg-7 col-lg-offset-0 col-md-7 col-md-offset-0 col-sm-7 col-sm-offset-0" style="background-image:url('<?php
           echo get_field('home_background_image')['url'];
