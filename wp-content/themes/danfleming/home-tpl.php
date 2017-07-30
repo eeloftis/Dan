548ee4a1ff54962f39fe3e42444970963be39aa8
Ema Loftis <eloftis@wellesley.edu>
2017-07-29 23:58:50 -0400

    fixed button link

--- wp-content/themes/danfleming/home-tpl.php
+++ wp-content/themes/danfleming/home-tpl.php
@@ -44,10 +44,10 @@ the_post(); ?>
        <?php if ($wp_query->current_post % 2 == 0): ?>
 
          <div class="projectCard odd row">
-           <div class="col-xs-10 col-xs-offset-1 col-lg-5 col-lg-offset-0 col-md-5 col-md-offset-0 col-sm-5 col-sm-offset-0"><img src="<?php
+           <div class="col-xs-10 col-xs-offset-1 col-lg-6 col-lg-offset-0 col-md-5 col-md-offset-0 col-sm-5 col-sm-offset-0"><img src="<?php
            echo get_field('home_image')['url'];
            ?>" ></div>
-           <div class="col-xs-10 col-xs-offset-1 col-lg-7 col-lg-offset-0 col-md-7 col-md-offset-0 col-sm-7 col-sm-offset-0" style="background-image:url('<?php
+           <div class="col-xs-10 col-xs-offset-1 col-lg-6 col-lg-offset-0 col-md-7 col-md-offset-0 col-sm-7 col-sm-offset-0" style="background-image:url('<?php
           echo get_field('home_background_image')['url'];
           ?>')">
              <span class="pull-right initials"><?php echo get_field('initials'); ?></span>
